14bef3ab4b05389f8a4458cc4b9330f953ca92c0
claytonhenrylewis <claytonhenrylewis@gmail.com>
2019-02-02 18:49:52 -0500

    Set up Google Cloud speech to text example

---
+++ speechtotext.js
@@ -0,0 +1,66 @@
+// Imports the Google Cloud client library.
+const {Storage} = require('@google-cloud/storage');
+const speech = require('@google-cloud/speech');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+
+// Set environment variable
+process.env.GOOGLE_APPLICATION_CREDENTIALS="noteable-d12e9bdafe4f.json";
+
+// Makes an authenticated API request.
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+const fs = require('fs');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+// The name of the audio file to transcribe
+const fileName = './resources/audio.raw';
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+  content: audioBytes,
+};
+const config = {
+  encoding: 'LINEAR16',
+  sampleRateHertz: 16000,
+  languageCode: 'en-US',
+};
+const request = {
+  audio: audio,
+  config: config,
+};
+
+// Detects speech in the audio file
+client
+  .recognize(request)
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
