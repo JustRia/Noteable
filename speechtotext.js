2ad991d59dc71ff7c79437ebb5da0b43c3eda177
claytonhenrylewis <claytonhenrylewis@gmail.com>
2019-02-05 21:20:39 -0500

    Add sample audio files

--- speechtotext.js
+++ speechtotext.js
@@ -31,7 +31,7 @@ const fs = require('fs');
 const client = new speech.SpeechClient();
 
 // The name of the audio file to transcribe
-const fileName = './resources/audio.raw';
+const fileName = './resources/audio-samples/male-misc/commercial_stereo.wav';
 
 // Reads a local audio file and converts it to base64
 const file = fs.readFileSync(fileName);
