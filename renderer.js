fbd0ab2dde093279966b43e9c65e8fa7bc15f1de
claytonhenrylewis <claytonhenrylewis@gmail.com>
2019-02-12 21:32:37 -0500

    Add a detect tempo button and event listener

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var detectTempoButton = document.getElementById("detect-tempo");
+detectTempoButton.addEventListener("click", detectTempo());
+
+function detectTempo () {
+
+}
