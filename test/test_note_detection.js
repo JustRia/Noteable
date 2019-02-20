6ac0eb682e96f9b3553ba0f5ee05ac776ab7882d
chang382 <changaaron286@gmail.com>
2019-02-20 14:22:19 -0500

    Beginning tests for measures_split() function

--- test/test_note_detection.js
+++ test/test_note_detection.js
@@ -52,3 +52,32 @@ describe('note_detection.js combine_notes() function unit tests', function() {
         expect(res).to.eql(expected);
     });
 });
+
+describe('note_detection.js measures_split() function unit tests', function() {
+    it('Function should round note lengths to the nearest multiple of 8', function() {
+        var beats_per_measure = 4;
+        var arr = [
+            { note_name_full : "c4", note : "c", octave : "4", note_length : 7 },
+            { note_name_full : "f4", note : "f", octave : "4", note_length : 18 },
+            { note_name_full : "a5", note : "a", octave : "5", note_length : 84 },
+        ];
+
+        var res = note_detect.measures(arr, beats_per_measure);
+
+        expect(res[0][0].note_length).to.eql(8);
+        expect(res[0][1].note_length).to.eql(16);
+        expect(res[0][2].note_length).to.eql(88);
+    });
+
+    it('Function should filter out note lengths below the lowest note threshold of 8 after rounding', function() {
+        var beats_per_measure = 4;
+        var arr = [
+            { note_name_full : "c4", note : "c", octave : "4", note_length : 4 },
+        ];
+
+        var res = note_detect.measures(arr, beats_per_measure);
+
+        //Should remove the c4 note since it's less than 8 when rounded
+        expect(res[0][0].note_name_full).to.not.eql("c4");
+    });
+});
