a9b27546c8e235321ec05ac5c67b2af2d76d4f4f
calebflynn13 <calebflynn13@gmail.com>
2019-02-20 15:51:17 -0500

    brought over tests from testing branch without breaking master

---
+++ test/recordingSpec.js
@@ -0,0 +1,40 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
+const chai = require('chai');
+
+let app
+describe('Record audio', function () {
+    this.timeout(10000)
+
+    before(function () {
+        app = new Application({
+            path: electronPath,
+            args: [path.join(__dirname, '..')]
+        })
+        return app.start()
+    })
+
+    after(function () {
+        if (app && app.isRunning()) {
+            return app.stop()
+        }
+    })
+
+    /* Microphone click tests */
+    it('mic button is visible', function () {
+        return app.client.element('#mic-icon').isVisible().then(function (isVisible) {
+            return assert.equal(isVisible, true)
+        })
+    });
+    it('record from mic icon works', function () {
+            return app.client.element('#mic-icon').click().then(function () {
+                return app.client.element('#mic-icon').click().then(function () {
+                    return assert.notEqual(null, app.client.element('#recordingsList'));
+                })
+            })
+        }
+
+    )
+})
