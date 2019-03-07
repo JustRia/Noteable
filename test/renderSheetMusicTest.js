605ce77f8e850ad0fc43f0ce8adbc82ba020189f
calebflynn13 <calebflynn13@gmail.com>
2019-03-07 13:09:38 -0500

    create test for rendering sheet music

---
+++ test/renderSheetMusicTest.js
@@ -0,0 +1,52 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+let app
+
+global.before(function() {
+    chai.should();
+    chai.use(chaiAsPromised);
+})
+
+describe('Render Sheet Music', function () {
+    this.timeout(10000)
+
+    before(function () {
+        app = new Application({
+            path: electronPath,
+            args: [path.join(__dirname, '..')]
+        })
+        return app.start()
+    })
+
+    after(function () {
+        // if (app && app.isRunning()) {
+        //     return app.stop()
+        // }
+    })
+
+    it('Sheet music renders correctly', function() {
+        return app.webContents.executeJavaScript('document.getElementById("sheet-music-main-content").classList.remove("hidden")', true).then(function() {
+            return app.webContents.executeJavaScript('document.getElementById("input-main-content").classList.add("hidden")', true).then(function() {
+                var bla = "var testArray = [" +
+                    "[" +
+                        "{ note_name_full : \"c4\", note : \"c\", octave : \"4\", accidental : undefined, freq : 0, note_length : 1 , note_type : [1]}," +
+                        "{ note_name_full : \"d4\", note : \"d\", octave : \"4\", accidental : undefined, freq : 0, note_length : 1 }," +
+                        "{ note_name_full : \"e4\", note : \"e\", octave : \"4\", accidental : undefined, freq : 0, note_length : 2 }," +
+                    "]," +
+                    "[" +
+                        "{ note_name_full : \"e4\", note : \"e\", octave : \"4\", accidental : undefined, freq : 0, note_length : 1 }," +
+                        "{ note_name_full : \"rest\", note : \"rest\", freq : 0, note_length : 3 }" +
+                    "]" +
+                "];";
+                app.webContents.executeJavaScript(bla, true);
+                app.webContents.executeJavaScript("renderSheetMusic(testArray);", true);
+                return true;
+            });
+        });
+    });
+});
