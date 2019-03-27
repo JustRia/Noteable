ba1167b838244ef0333bc905135be4d21a1bcb39
calebflynn13 <calebflynn13@gmail.com>
2019-03-27 15:10:50 -0400

    add metronome to screen

--- renderer.js
+++ renderer.js
@@ -40,6 +40,8 @@ function startRecording() {
         if (!document.getElementById("mic-icon").classList.contains("disabled-button")) {
             document.getElementById("mic-icon").classList.add("hidden");
             document.getElementById("stop-icon").classList.remove("hidden");
+            document.getElementById("metronome-main-content").classList.remove("hidden");
+            document.getElementById("input-main-content").classList.add("hidden");
 
             recording = true;
 
@@ -172,7 +174,7 @@ function startMetronome() {
     var text = document.createTextNode(tempo_input + " BPM");
     circ.appendChild(text);
 
-    //start the countdown based on the tempo 
+    //start the countdown based on the tempo
     timer = window.setInterval(function () { //decrement on the beat?
         document.getElementById("countdown").innerHTML = "" + countFrom;
         if (countFrom == 0) {
